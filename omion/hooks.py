30e2cb86654e53d5aec83dde574ba8931815a7d8
root <viral@fosserp.com>
2024-01-30 13:53:14 +0000

    ch

--- omion/hooks.py
+++ omion/hooks.py
@@ -27,7 +27,7 @@ app_license = "[A"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Supplier" : "public/js/supplier.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -124,7 +124,9 @@ app_license = "[A"
 # 	}
 # }
 doc_events = {
-    "Supplier":"omion.omopn.docevents.supplier.create_vender_series"
+    "Supplier":{
+        "validate":"omion.omopn.docevents.supplier.create_vender_series"
+    }
 }
 
 
--- omion/public/js/supplier.js
+++ omion/public/js/supplier.js
@@ -1,12 +1,14 @@
 frappe.ui.form.on('Supplier', {
     validate:function(frm){
         if(frm.doc.custom_vendor_reg_number){
-            frm.set_df_property('custom_vendor_reg_number', 'reqd', 1)
+            console.log("kkk")
+            frm.set_df_property('custom_vendor_reg_number', 'read_only', 1)
         }
     },
     refresh:function(frm){
         if(frm.doc.custom_vendor_reg_number){
-            frm.set_df_property('custom_vendor_reg_number', 'reqd', 1)
+            console.log('jj')
+            frm.set_df_property('custom_vendor_reg_number', 'read_only', 1)
         }
     }
 })
