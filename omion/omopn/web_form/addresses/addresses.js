01a035473bfd537428e666778ef009315a2ac96a
viral <viral@fosserp.com>
2024-03-17 09:26:49 +0000

    ch

---
+++ omion/omopn/web_form/addresses/addresses.js
@@ -0,0 +1,5 @@
+frappe.ready(function() {
+	window.frappe.web_form.set_value('custom_user', frappe.session.user)
+
+	
+})
---
+++ omion/omopn/web_form/addresses/addresses.py
@@ -0,0 +1,6 @@
+import frappe
+
+def get_context(context):
+	# do your magic here
+	# context.show_sidebar = True
+	pass
